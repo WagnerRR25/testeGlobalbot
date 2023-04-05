8763eb5209f8b408cfee569a8715aa4b7e16da28
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-04-04 23:21:12 -0300

    conectado ao MongoBD

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
-
 const express = require('express')
 const app = express()
+
+const mongoose = require('mongoose')
+
+
+
 
 app.use(
     express.urlencoded({
@@ -13,7 +17,21 @@ app.use(express.json())
 
 app.get('/', (req,res) => (
 
-    res.status(200).json({ message: 'Hello express'})
+    res.status(200).json({ message: 'Hello express!'})
 ))
 
-app.listen
+const DB_USER = 'WagnerRR25'
+const DB_PASSWORD = encodeURIComponent('QaMMRJ3LDt3Np2qA')
+
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apirestfulcluster.unjmnpv.mongodb.net/?retryWrites=true&w=majority`
+).then(() => {
+    console.log('conectado ao MongoDB!')
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
+
+
+
+
